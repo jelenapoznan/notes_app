ef1d5cdc9fcd714e267b16c51e6ba390b502bc52
Jelena Poznan <jelenahipi@gmail.com>
2024-07-08 12:56:41 +0200

    sign up post request and message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from flask_login import current_user
 
 # Setting up Blueprint for Flask application
@@ -15,4 +15,24 @@ def logout():
 
 @auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    firstName = request.form.get('firstName')
+    lastName = request.form.get('lastName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+    elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+    elif len(lastName) < 2:
+            flash('Last name must be greater than 1 character.', category='error')
+    elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+    elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+    else:
+         flash('Account created!', category='success')
+
   return render_template('signup.html', user=current_user)
