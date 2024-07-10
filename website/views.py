06dd8e1994c5843c068d57a1f014f5f75529299c
Jelena Poznan <jelenahipi@gmail.com>
2024-07-10 13:40:08 +0200

    adding user notes

--- website/views.py
+++ website/views.py
@@ -1,14 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import current_user, login_required
+from .models import Note
+from . import db
 
 # Setting up Blueprint for Flask application, name of this blueprint is views
 views = Blueprint('views', __name__)
 
 # Defining rout 
-@views.route('/')
+@views.route('/', methods = ['GET', 'POST'])
 @login_required
 # This f will run whenever we go to / route
 def home():
+  if request.method == 'POST':
+    note = request.form.get('note')
+
+    if len(note) < 1:
+      flash('Note is too short!', category = 'error')
+    else:
+      new_note = Note(data = note, user_id = current_user.id)
+      db.session.add(new_note)
+      db.session.commit()
+      flash('Note added!', category = 'sucecss')
   return render_template('home.html', user=current_user)
 
 
