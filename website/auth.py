41cb133b83cbe842c5760799ba3893e2bad24876
Jelena Poznan <jelenahipi@gmail.com>
2024-07-10 10:26:20 +0200

    logging in users

--- website/auth.py
+++ website/auth.py
@@ -10,6 +10,23 @@ auth = Blueprint('auth', __name__)
 # Defining routes
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
+  if request.method == 'POST':
+       email = request.form.get('email')
+       password = request.form.get('password')
+
+       # Check if the user email is valid
+       user = User.query.filter_by(email = email).first()
+       if user:
+             if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='seccess')
+                return redirect(url_for('views.home'))
+             else:
+                 flash('Incorrect password, try again!', category='error')
+       else:
+            flash('Email does not exist.', category='error')
+
+
+
   return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
@@ -25,7 +42,11 @@ def sign_up():
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
 
-    if len(email) < 4:
+    user = User.query.filter_by(email = email).first()
+    # Making shure that we are not singing up user with the same email
+    if user:
+         flash('Email already exist.', category='error')
+    elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
     elif len(firstName) < 2:
             flash('First name must be greater than 1 character.', category='error')
